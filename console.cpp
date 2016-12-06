5120d7804e6dd4aa4797422673c01da01954a91a
Aart Odding <aart_odding@hotmail.com>
2016-12-07 00:29:15 +0100

    Create namespace console for input/ output

--- console.cpp
+++ console.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
-#include "input.h"
+#include "console.h"
+#include "utility.h"
+
 
 
 // returns 0 if no valid letter or number was choosen
-char input::getDecision() {
+char console::getDecision() {
 	std::string input;
 	std::getline(std::cin, input);
 	for (int i = 0; i < input.size(); ++i) {
 		char test{ input[i] };
-		if (inside(test, 48, 57) || inside(test, 65, 90) || inside(test, 97, 122)) return test;
+		if (utility::inside(test, 48, 57) || utility::inside(test, 65, 90) || utility::inside(test, 97, 122)) return test;
 	}
 	return 0;
 }
--- utility.h
+++ utility.h
@@ -1,9 +1,7 @@
 #pragma once
 
 
-namespace input{
-
-	char getDecision();
+namespace utility {
 
 	template<typename T1, typename T2, typename T3>
 	bool inside(T1 value, T2 min, T3 max) {
